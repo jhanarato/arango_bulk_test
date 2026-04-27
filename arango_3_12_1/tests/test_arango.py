52e5010c95b73b8d2046a5ce5bb168baa58cb04b
jr <jhanarato@gmail.com>
2026-04-27 18:13:11 +0800

    Add test for collection creation.

--- arango_3_12_1/tests/test_arango.py
+++ arango_3_12_1/tests/test_arango.py
@@ -6,14 +6,33 @@ DB_NAME = 'bulk_load_test'
 
 @pytest.fixture
 def client() -> ArangoClient:
-    return ArangoClient(host="old-arango-db", port=8529, username='root', password='test')
+    return ArangoClient(host='old-arango-db', port=8529, username='root', password='test')
 
 
 @pytest.fixture
 def database(client):
     client.create_database(DB_NAME)
-    yield
+    yield client.database(DB_NAME)
     client.delete_database(DB_NAME)
 
-def test_database(client, database):
-    assert DB_NAME in client.databases()
+def test_database(database):
+    assert database.name == DB_NAME
+
+class TestImportBulk:
+    def test_populate_a_collection(self, database):
+        cats_collection = database.create_collection('cats')
+        cats = [
+            {
+                "_key": "felicity",
+                "name": "Felicity",
+                "breed": "Siamese",
+            },
+            {
+                "_key": "prince",
+                "name": "Prince",
+                "breed": "Shorthair",
+            },
+
+        ]
+        cats_collection.import_bulk(cats)
+        assert cats_collection.count() == 2
